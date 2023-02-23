89d564b7c242a967dea4cefda00d273a856bcc8d
caojunjie <1301239018@qq.com>
2023-02-23 08:38:44 +0800

    feat(limits) 各个类型字节长度、int的最大值

--- c++primer/03.handle-data/01.limits.cpp
+++ c++primer/03.handle-data/01.limits.cpp
@@ -5,11 +5,13 @@ int main()
 {
   using namespace std;
   int n_int = INT_MAX;
+  int un_int = UINT_MAX;
   short n_short = SHRT_MAX;
   long n_long = LONG_MAX;
   long long n_llong = LLONG_MAX;
   // sizeof operator yields size of type or of variable
   cout << "int is  " << sizeof(int) << " bytes" << endl;
+  cout << "un_int is  " << sizeof(un_int) << " bytes" << endl;
   cout << "short is " << sizeof(n_short) << " bytes, " << endl;
   cout << "n_long is " << sizeof(n_long) << " bytes." << endl;
   cout << "n_llong  is " << sizeof(n_llong) << " bytes," << endl;
@@ -17,11 +19,25 @@ int main()
 
   cout << "Maximum values:" << endl;
   cout << "int: " << n_int << endl;
+  cout << "un_int: " << un_int << endl;
   cout << "short: " << n_short << endl;
   cout << "long: " << n_long << endl;
   cout << "long long : " << n_llong << endl;
 
+  cout << endl;
+
   cout << "Minimum int value = " << INT_MIN << endl;
   cout << "bits per byte = " << CHAR_BIT << endl;
+
+  // 2的32次方 0 ～ 4294967295
+  unsigned int max_uint = 4294967295;
+  // -2147483647 ~ 2147483647
+  int max_int = 2147483647;
+
+  long also = 560334;
+
+  cout << "max_uint: " << max_uint << endl;
+  cout << "max_int: " << max_int << endl;
+  cout << "also: " << also << endl;
   return 0;
 }
--- c++primer/README.md
+++ c++primer/README.md
@@ -10,3 +10,8 @@ g++ xxx.cpp
 // 运行可执行文件
 ./a.out
 ```
+
+## question
+
+- int is  4 bytes
+  - 那int 能定义的最长长度是多少位？目前测试下来是9位。为什么？
