0b0d2023ec8922331525ec041d49fa7931d9fb46
caojunjie <1301239018@qq.com>
2023-02-26 21:00:22 +0800

    feat(arraynew)

--- c++primer/04.composite-type/07-3.arraynew.cpp
+++ c++primer/04.composite-type/07-3.arraynew.cpp
@@ -8,6 +8,7 @@ int main(int argc, char const *argv[])
   p3[0] = 0.2;
   p3[1] = 0.5;
   p3[2] = 0.8;
+  cout << "whole p3 address " << &p3 << endl;
   cout << "p3 address: " << p3 << " p3[1] is " << p3[1] << ".\n";
   p3 = p3 + 1;
   cout << "Now p3 address : " << p3 << " p3[0] is " << p3[0] << " and ";
