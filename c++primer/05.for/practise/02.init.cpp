6c239847337311a532f4fb5a6a710aa8d067a5c8
caojunjie <1301239018@qq.com>
2023-03-10 10:17:56 +0800

    feat(clang) for循环

---
+++ c++primer/05.for/practise/02.init.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  using namespace std;
+
+  int x = (1224);
+  cout << x << endl;
+  int y;
+  y = 1024;
+  cout << y << endl;
+  return 0;
+}
