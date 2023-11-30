b99fc66ec58c6be7551fd7b01cad16135e1a3a4b
kevinbrother <1301239018@qq.com>
2023-11-30 22:44:06 +0800

    feat(link) 手动链接源文件

---
+++ base/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "sum.h"
+#include "test.txt"
+
+int main(int argc, char const *argv[])
+{
+
+  printf("x: %d\n", x);
+  printf("自定义: %d\n", 1);
+  printf("sum(1,2 ): %d", sum(1, 2));
+  return 0;
+}
