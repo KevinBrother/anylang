264f0ad2d4d0d1280b25b985fb877accc9495d9f
kevinbrother <1301239018@qq.com>
2023-11-30 22:28:47 +0800

    feat(include) 添加预处理指令

--- base/printf.c
+++ base/printf.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "test.txt"
 
-int plus_one(int n) {
+int plus_one(int n)
+{
     return n + 1;
 };
 
-int main(void){
+int main(void)
+{
     // 1. printf
     printf("hello world \a \r\tdsaa \r");
 
@@ -12,26 +15,25 @@ int main(void){
     int x = 5;
     printf("x's address is %p\n", &x);
 
-    // 2. 
-     printf("指针5的地址的值为 %d \n",*(&x));
-    /*  
+    // 2.
+    printf("指针5的地址的值为 %d \n", *(&x));
+    /*
     if (x == *(&x)) {
         printf('right \n');
     } */
 
     // 3. 函数调用
-    
+
     int funRst = plus_one(12);
     printf("plus_one的值 %d \n", funRst);
-
 
     // 4. 数组
     // TODO 这个值为啥是12??
     int arr[] = {22, 37, 11};
     int arrLen = sizeof(arr); // 12
 
-    printf("arrlen的sizeof %d \n",arrLen);
+    printf("arrlen的sizeof %d \n", arrLen);
 
-    // 
+    //
     return 0;
 }
