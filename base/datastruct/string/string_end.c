4b8b9bd1a228514558dd09b2abee4b0403078bf0
kevinbrother <1301239018@qq.com>
2023-12-05 23:44:30 +0800

    feat(string_end) 字符串结束符

---
+++ base/datastruct/string/string_end.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char a[3] = {'m', 'j', '\0'}; // 添加了结束符\0
+    char b[] = {'i', 's'}; // 假设忘记添加结束符\0
+    printf("字符串a：%s", a); // 输出字符串a
+    printf("\n"); // 换行
+    printf("字符串b：%s", b); // 输出字符串b
+
+    return 0;
+}
