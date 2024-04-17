ba5ae9ba122a6abe2b4a39827b6b042b36374db1
caojunjie <1301239018@qq.com>
2024-04-17 15:49:03 +0800

    feat(10) 初始化指针注意点

--- c_primer_plus/10.array/05.ptr-opt.c
+++ c_primer_plus/10.array/05.ptr-opt.c
@@ -35,12 +35,21 @@ int main() {
 
     // 一个指针减去另一个指针
     printf("\n subtracting one pointer from another:\n");
-    printf("ptr2 = %p, ptr1 = %p, ptr2 - ptr1 = %td\n", ptr2, ptr1, ptr2 - ptr1);
+    printf("ptr2 = %p, ptr1 = %p, ptr2 - ptr1 = %td\n", ptr2, ptr1, ptr2 - ptr1); // 当前两个指针指向的元素相隔两个int 而不是2个字节
 
     // 一个指针减去一个整数
     printf("\n subtracting a int from a pointer:\n");
     printf("ptr3 = %p, ptr3 - 2 = %p\n", ptr3, ptr3 - 2);
 
     return 0;
+
+}
+
+
+void error() {
+    // 系统值分配了存储指针本身的内存，但是没有分配指针指向的内存，所以不能对指针进行解引用
+    // 系统值分配了存储指针本身的内存，但是没有分配存储数据的内存。
+    int *p; // 可以使用 一个现有变量的地址初始化该指针，或使用 malloc() 函数先分配内存。
+    *p = 5; // 错误，试图对一个空指针解引用
 
 }
