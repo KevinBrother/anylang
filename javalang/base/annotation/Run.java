8a27ce3735a4f1437f90433b4e2aff41c35a0659
kevinbrother <1301239018@qq.com>
2024-08-31 22:43:28 +0800

    feat(annotation)

---
+++ javalang/base/annotation/Run.java
@@ -0,0 +1,18 @@
+package annotation;
+
+public class Run {
+    @SourcePolicy
+	public void sourcePolicy() {
+	}
+ 
+	@ClassPolicy
+	public void classPolicy() {
+	}
+ 
+	@RuntimePolicy
+	public void runtimePolicy() {
+	}
+    public static void main(String[] args) {
+		System.out.printf("12");
+    }
+}
