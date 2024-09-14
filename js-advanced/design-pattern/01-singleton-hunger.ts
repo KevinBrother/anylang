c6bac81fa34a020a569f118de16ba72ab7b8aad1
caojunjie <1301239018@qq.com>
2024-09-14 17:21:51 +0800

    feat(01) 工厂模式

---
+++ js-advanced/design-pattern/01-singleton-hunger.ts
@@ -0,0 +1,45 @@
+// 小明去了一家大型商场，拿到了一个购物车，并开始购物。
+// 请你设计一个购物车管理器，记录商品添加到购物车的信息（商品名称和购买数量），
+// 并在购买结束后打印出商品清单。（在整个购物过程中，小明只有一个购物车实例存在）
+
+interface IProduct {
+  name: string;
+  quantity: number;
+}
+
+interface IShoppingCartManager {
+  add(name: string, quantity: number);
+  viewCart();
+}
+
+class ShoppingCartManager implements IShoppingCartManager {
+  private static instance = new ShoppingCartManager();
+  private products: IProduct[] = [];
+
+  private constructor() {}
+
+  static getInstance() {
+    return ShoppingCartManager.instance;
+  }
+
+  add(name: string, quantity: number) {
+    this.products.push({ name, quantity });
+  }
+
+  viewCart() {
+    this.products.forEach(({ name, quantity }) => {
+      console.log(name, quantity);
+    });
+  }
+}
+
+(function () {
+  const instance = ShoppingCartManager.getInstance();
+
+  instance.add("Apple", 3);
+  instance.add("Banana", 2);
+  instance.add("Orange", 5);
+  instance.viewCart();
+})();
+
+export {};
