ac1ecdc9121e5a16f0f7a3693697afe0eb6c7a6f
caojunjie <1301239018@qq.com>
2024-09-24 13:43:24 +0800

    feat(proxy): 代理模式

---
+++ js-advanced/design-pattern/utils.ts
@@ -0,0 +1,11 @@
+export function entry(count: number, fn: (...args: any) => void) {
+    function dfs(...args) {
+      if (args.length < count) {
+        return (...arg) => dfs(...args, ...arg);
+      }
+  
+      fn(...args)
+    }
+    return dfs;
+  }
+  
