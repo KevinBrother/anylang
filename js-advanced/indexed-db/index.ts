38e74a0b47a2114944b693d54dd42ce46cdb9132
caojunjie <1301239018@qq.com>
2024-11-25 17:17:19 +0800

    feat(done)

--- js-advanced/indexed-db/index.ts
+++ js-advanced/indexed-db/index.ts
@@ -5,6 +5,7 @@ import {
   getMany,
   set,
   get,
+  keys,
   update,
 } from "idb-keyval";
 // store 和 table 是同一个概念
@@ -13,8 +14,8 @@ const kssStore = createStore("kss-db", "kss-store");
 // 增删改查
 
 // 添加
-set("kss-obj", { name: "obj1", id: 1, age: 18 }, kssStore);
-set("kss-obj", { name: "obj1", id: 2, age: 18 }, kssStore);
+set("1", { name: "obj1", id: 1, age: 18 }, kssStore);
+set("2", { name: "obj1", id: 2, age: 18 }, kssStore);
 
 // 修改
 // update("kss-obj", (value) => ({ ...value, name: "obj3" }), kssStore);
@@ -30,3 +31,11 @@ get("kss-obj", kssStore)
   .catch((error) => {
     console.log("error", error);
   });
+
+keys(kssStore).then((keys) => {
+  console.log("keys", keys);
+});
+
+entries(kssStore).then((entries) => {
+  console.log("entries", entries);
+});
