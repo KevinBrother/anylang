7615630af3e21c5ffef740b47b173dcb80568972
caojunjie <1301239018@qq.com>
2024-12-19 16:18:28 +0800

    http(update)

---
+++ go/base/05.http/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	c := make(chan string)
+
+	go func(c chan string) {
+		var port = "8081"
+
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "hello, %q", r.URL.Path)
+		})
+
+		http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, World! %q", r.URL.Path)
+		})
+
+		http.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
+			os.Exit(0)
+		})
+
+		serverUrl := "http://localhost:" + port
+		c <- serverUrl
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			fmt.Println("Error starting server:", err)
+		}
+
+	}(c)
+
+	serverUrl := <-c
+	fmt.Println("Server started at", serverUrl)
+
+	select {} // Block forever
+}
