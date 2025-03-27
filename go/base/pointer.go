3b58587b74899d3a7d848f103cafd082ab4ccc94
caojunjie <1301239018@qq.com>
2025-03-27 16:56:05 +0800

    doc(pointer): 传值与传指

--- go/base/pointer.go
+++ go/base/pointer.go
@@ -2,80 +2,40 @@ package main
 
 import "fmt"
 
-type myStruct struct {
-	name string
-}
-
+// Person 结构体定义，包含一个切片类型的属性
 type Person struct {
-	name string
-	age  int
-	// children []Children //（值类型切片）每次添加或修改时，都会复制整个结构体， 修改切片中的 Children 不会影响原始的 Children 变量。
-	children []*Children // （指针切片）切片中存储的是指向 Children 结构体的指针，修改切片中元素的字段会直接影响原始的 Children 实例。
+	name    string
+	age     int
+	myMap   map[string]string
+	array   *[2]int
+	mySlice *[]string
 }
 
-type Children struct {
-	cname string
-	cage  int
-}
-
-func swap(a, b myStruct) {
-	tmp := a
-	a = b
-	b = tmp
-	fmt.Printf("p1 = %v, p2 = %v\n", a, b)
-}
-
-func swap2(a, b *myStruct) {
-	fmt.Printf("a = %p\n", &a)
-	fmt.Printf("a  = %p\n", a)
-
-	tmp := *a
-	*a = *b
-	*b = tmp
-	fmt.Printf("p1 = %v, p2 = %v\n", a, b)
+// valueStruct 接收 Person 类型的值
+func valueStruct(p Person) {
+	fmt.Printf("hoobie: %p\n", p.mySlice)
+	p.array[0] = 1
+	p.array[0] = 2
+	p.myMap["key1"] = "val1"
+	*p.mySlice = append(*p.mySlice, "reading")
+	fmt.Printf("Inside valueStruct: mySlice = %v\n", *p.mySlice)
+	fmt.Printf("Inside valueStruct: array = %v\n", p.array)
+	fmt.Printf("Inside valueStruct: myMap = %v\n", p.myMap)
 }
 
 func main() {
 
-	var p1 myStruct
-	p1.name = "p1_name"
-
-	var p2 myStruct
-	p2.name = "p2_name"
-
-	swap(p1, p2)
-	// fmt.Printf("p1 = %v, p2 = %v\n", p1, p2)
-
-	swap2(&p1, &p2)
-	fmt.Printf("&p1 = %p\n", &p1)
-
-	// fmt.Printf("p1 = %v, p2 = %v\n", p1, p2)
-	c1 := Children{
-		cname: "c1",
-		cage:  12,
+	po2 := Person{
+		name:    "p2",
+		age:     12,
+		myMap:   make(map[string]string),
+		array:   &[2]int{},
+		mySlice: &[]string{"swimming"},
 	}
 
-	po1 := Person{name: "p1", age: 12, children: []*Children{
-		&c1,
-	}}
-	po2 := Person{name: "p2", age: 12}
-
-	fmt.Printf("po1 addr: %p \n", &po1)
-	fmt.Printf("po2 addr: %p \n", &po2)
-
-	pointStruct(&po1)
+	fmt.Printf("hoobie: %p\n", po2.mySlice)
 	valueStruct(po2)
-
-	fmt.Printf("po1.name: %s \n", po1.name)
-	fmt.Printf("po2.name: %s \n", po2.name)
-}
-
-func pointStruct(p *Person) {
-	fmt.Printf("[在这里修改值会影响到原始值， 对内存影响小] p in pointStruct addr, &p: %p ,p: %p \n", &p, p)
-	p.name = "inner pointStruct p"
-}
-
-func valueStruct(p Person) {
-	fmt.Printf("[在这里修改值不会影响到原始值， 对内存影响大] p in valueStruct addr, &p: %p ,p: %p \n", &p, p)
-	p.name = "inner valueStruct p"
+	fmt.Printf("Outside valueStruct: mySlice = %v\n", po2.mySlice)
+	fmt.Printf("Outside valueStruct: array = %v\n", po2.array)
+	fmt.Printf("Outside valueStruct: map = %v\n", po2.myMap)
 }
