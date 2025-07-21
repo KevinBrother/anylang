42c35b58c865c16da2dc92279e2ecb4d466c4ec2
Kevinbrother <1301239018@qq.com>
2025-07-21 12:21:55 +0800

    feat(map)

---
+++ go/base/test/books/02_m_test.go
@@ -0,0 +1,33 @@
+package books
+
+import (
+	"net/http"
+	_ "net/http/pprof"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestCAS(t *testing.T) {
+
+	var a int64 = 12
+
+	atomic.CompareAndSwapInt64(&a, 12, 3)
+}
+
+func TestDeadLock(t *testing.T) {
+	var mu sync.Mutex
+	c := 0
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			mu.Lock()
+			defer mu.Unlock()
+			c++
+		}()
+	}
+
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
+		panic(err)
+	}
+}
