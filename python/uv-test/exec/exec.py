aefbd8714cbfcf4c54b76670291ac373abf98396
caojunjie <1301239018@qq.com>
2026-02-24 18:26:21 +0800

    feat: module

---
+++ python/uv-test/exec/exec.py
@@ -0,0 +1,10 @@
+# def add(a, b):
+#     """Returns the sum of a and b."""
+#     return a + b
+
+# from utils.add import add
+from utils import add
+
+def exec_main():
+    print("Hello from uv-test exec!")
+    print(add.add(1, 2))
--- python/uv-test/main.py
+++ python/uv-test/main.py
@@ -1,6 +1,11 @@
+from utils.add import add
+from exec import exec
+
 def main():
     print("Hello from uv-test!")
-
+    print(add(1, 2))
+    exec.exec_main()
+    print(exec.add.add(3, 4))
 
 if __name__ == "__main__":
     main()
