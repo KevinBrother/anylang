e8adc6b594466343d5ed4acf4edd61b31c20a70f
caojunjie <1301239018@qq.com>
2026-02-25 13:03:19 +0800

    feat: test

--- python/base/test/test_pytest.py
+++ python/base/test/test_pytest.py
@@ -1,6 +1,10 @@
 import pytest
+import sys
+print(sys.path)
+from  src import utils
+
 
 def test_add():
-    assert 2 + 3 == 5
-    assert -1 + 1 == 0
+    assert utils.add(2, 3) == 5
+    assert utils.add(-1, 1) == 0
     
--- python/base/test/test_unittest.py
+++ python/base/test/test_unittest.py
@@ -1,10 +1,11 @@
 import unittest
+from src import utils
 
 class TestAdd(unittest.TestCase):
     def test_add(self):
-        # self.assertEqual(2 + 3, 6, "Should be 5")
-        self.assertEqual(2 + 3, 5, "Should be 5")
-        self.assertEqual(-1 + 1, 0)
+        self.assertEqual(utils.add(2, 4), 6, "Should be 6")
+        self.assertEqual(utils.add(2, 3), 5, "Should be 5")
+        self.assertEqual(utils.add(-1, 1), 0)
 
 if __name__ == '__main__':
     unittest.main()
