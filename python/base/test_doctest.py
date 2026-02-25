3d31e3150470bcdc535ba482a8df75b6922269e7
caojunjie <1301239018@qq.com>
2026-02-25 10:59:04 +0800

    doctest

---
+++ python/base/test_doctest.py
@@ -0,0 +1,24 @@
+"""
+This is the "example" module.
+
+The example module supplies one function, add().  For example,
+
+>>> add(3, 3)
+6
+"""
+
+def add(a, b):
+    """
+    >>> add(2, 3)
+    5
+    >>> add(-1, 1)
+    0
+    >>> add(0, 0)
+    0
+    """
+    return a + b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
