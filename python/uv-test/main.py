f2bcf97ff1a7f4840d7e460ce93970fcc6f3b65d
caojunjie <1301239018@qq.com>
2026-02-25 13:49:17 +0800

    module

--- python/uv-test/main.py
+++ python/uv-test/main.py
@@ -1,11 +1,12 @@
 from utils.add import add
-from exec import exec
+from exec import add as exec_add
+import exec.exec as exec_module
 
 def main():
     print("Hello from uv-test!")
     print(add(1, 2))
-    exec.exec_main()
-    print(exec.add.add(3, 4))
+    exec_module.exec_main()
+    print(exec_add.add(3, 4))
 
 if __name__ == "__main__":
     main()
