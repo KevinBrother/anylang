d562cecef00a712d0ca9c8320699c1008f34b81c
caojunjie <1301239018@qq.com>
2026-02-26 12:26:09 +0800

    module relative path

--- python/uv-test/pkg/__init__.py
+++ python/uv-test/pkg/__init__.py
@@ -1,4 +1,6 @@
-from pkg.t1 import t1fn
-from pkg.t2 import t2fn
+from pkg.t2 import t2fn 
+from .t1 import t1fn # . 相对路径目前目录中更合适
 
-__all__ = ["t1fn", "t2fn"]
+
+# from your_module import * 导入你的模块时，Python 只导入 __all__ 列表里列出的名字
+# __all__ = ["t1fn", "t2fn"]
